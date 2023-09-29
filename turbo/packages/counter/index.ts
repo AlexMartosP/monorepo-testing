eb94325c91c0809b98cd29b6aa04fa15895f3bdf
Alexander MP <alex.martos@hotmail.se>
2023-09-29 13:59:52 +0200

    Added turbo and changed gitignore

---
+++ turbo/packages/counter/index.ts
@@ -0,0 +1,20 @@
+import log from "lib-of-turbo";
+
+let currentCount = 0;
+
+function count() {
+  currentCount += 1;
+}
+
+function getCount() {
+  return currentCount;
+}
+
+count();
+count();
+count();
+
+log("From counter package");
+log(getCount());
+
+export default { count, getCount };
